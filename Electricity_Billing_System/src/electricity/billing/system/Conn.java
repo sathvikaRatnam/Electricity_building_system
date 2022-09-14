804af549cbfbcccb2cdadf5c50a43699b20dfea0
unknown <sathvikaratnam@gmail.com>
2022-09-14 12:22:32 +0530

    Electricity_Billing_System

---
+++ Electricity_Billing_System/src/electricity/billing/system/Conn.java
@@ -0,0 +1,19 @@
+
+package electricity.billing.system;
+
+
+import java.sql.*;
+
+public class Conn {
+
+    Connection c;
+    Statement s;
+    Conn() {
+        try {
+            c = DriverManager.getConnection("jdbc:mysql:///ebs", "root", "ratnam@15");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
